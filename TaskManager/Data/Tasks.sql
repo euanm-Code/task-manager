fb0431361e500f9fa7a1425fe1fff05dc78c7109
Euan Mair <euan.mair@outlook.com>
2018-03-04 18:17:36 +0000

    Further setup of the project.

---
+++ TaskManager/Data/Tasks.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Date] DATETIME NOT NULL, 
+    [AssignedUser] NVARCHAR(50) NULL, 
+    [Created] DATE NOT NULL, 
+    [Modified] DATETIME NOT NULL, 
+    [ModifiedBy] NVARCHAR(50) NOT NULL, 
+    [CreatedBy] NVARCHAR(50) NOT NULL
+)
---
+++ TaskManager/Data/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(80) NOT NULL, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL
+)
